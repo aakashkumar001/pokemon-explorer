421c68da64a24b76ef02474418a194326fb0fea9
aakashkr <aspakash000@gmail.com>
2024-07-19 15:18:01 +0530

    prisma-config2

--- server/routers/pokemonRouter.ts
+++ server/routers/pokemonRouter.ts
@@ -1,6 +1,6 @@
 import {publicProcedure, router} from "../trpc";
 import {z} from "zod";
-import {prisma} from "../../lib/prisma"
+import prisma from "../../lib/prisma"
 
 
 export const pokemonRouter = router({
